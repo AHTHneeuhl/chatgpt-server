f66d5cc4dcf066fedd2c62b2330adddf66683816
Shubham Patrick <ahthneeuhl@gmail.com>
2023-03-07 23:26:16 +0530

    Configurations, server and OpenAI routes setup

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+import { Configuration, OpenAIApi } from "openai";
+import openaiRoutes from "./routes/openai.js";
 
 // Configurations
 dotenv.config();
@@ -15,6 +17,15 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+// OpenAI configurations
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+export const openai = new OpenAIApi(configuration);
+
+// Routes
+app.use("/openai", openaiRoutes);
 
 // Server setup
 const PORT = process.env.PORT || 9000;
